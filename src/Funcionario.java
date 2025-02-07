fac886df21cfab36340e331d3ec52b8d196c24a6
Natal Lima <nataldelima@gmail.com>
2025-02-07 13:50:23 -0400

    Commit inicial do Projeto

---
+++ src/Funcionario.java
@@ -0,0 +1,34 @@
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa {
+    private BigDecimal salario;
+    private String funcao;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String funcao, BigDecimal salario, String nome, LocalDate dataNascimento) {
+        super(nome, dataNascimento);
+        this.funcao = funcao;
+        this.salario = salario;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+}
